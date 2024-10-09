164abd35f3adb5827dfa09bcf1dbab0ea97ef32d
angelornellab <angel.ornellab@gmail.com>
2024-10-09 17:36:44 -0300

    feat: user-managers

---
+++ user-managers/src/App.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+
+import Card from './components/Card';
+import Container from './components/Container';
+import UserForm from './components/UserForm';
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const submit = user => {
+    setUsers([
+      ...users,
+      user,
+    ]);
+  };
+  
+  console.log(users);
+  return (
+    <div style={{marginTop: '15%'}}>
+      <Container>
+        <Card>
+          <div style={{padding:20}}>
+            <UserForm submit={submit}/>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {
+              users.map(user => 
+                <li key={user.email}>{`${user.name} ${user.lastName}: ${user.email}`}</li>
+              )
+            }
+          </ul>
+      </Card>
+    </Container>
+    </div>
+  );
+}
+
+export default App;
